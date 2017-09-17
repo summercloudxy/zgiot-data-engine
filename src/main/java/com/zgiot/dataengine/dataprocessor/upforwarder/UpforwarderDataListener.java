b112a60dccf9e0aff46b122fb69405941ed483f1
jeickey <jeickey@163.com>
2017-09-17 14:34:45 +0800

    support multi wss sessions.

--- src/main/java/com/zgiot/dataengine/dataprocessor/upforwarder/UpforwarderDataListener.java
+++ src/main/java/com/zgiot/dataengine/dataprocessor/upforwarder/UpforwarderDataListener.java
@@ -54,21 +54,26 @@ public class UpforwarderDataListener implements DataListener {
     }
 
     private void flushBuffer(BlockingQueue<DataModel> buffer) {
+        if (sessions.size()==0){
+            return ;
+        }
+
+        List<DataModel> list = new ArrayList(buffer.size()+1000);
+        buffer.drainTo(list);
+        // get from buffer and send them
+        errorCounter.set(0);
+
+        if (list.size() == 0) {
+            return; // no data and cancel this flush
+        }
+
+        // do send to all sessions
         for (WebSocketSession session : sessions) {
             synchronized (session) {
                 // send message
                 if (!session.isOpen()) {
                     logger.warn("Websocket session '{}' is closed ", session);
                     continue;
-                }
-
-                // get from buffer and send them
-                List<DataModel> list = new ArrayList(buffer.size()+1000);
-                buffer.drainTo(list);
-                errorCounter.set(0);
-
-                if (list.size() == 0) {
-                    break; // cancel this flush
                 }
 
                 for (DataModel dm : list) {
