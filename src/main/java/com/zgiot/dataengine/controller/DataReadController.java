6793e2db2c635207e020974d7856c1e896ed3b53
jeickey <jeickey@163.com>
2017-10-21 15:25:14 +0800

    change to value only.

---
+++ src/main/java/com/zgiot/dataengine/controller/DataReadController.java
@@ -0,0 +1,34 @@
+package com.zgiot.dataengine.controller;
+
+import com.zgiot.common.pojo.DataModel;
+import com.zgiot.common.restcontroller.ServerResponse;
+import com.zgiot.dataengine.dataplugin.kepserver.KepServerDataPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/data")
+public class DataReadController {
+
+    @Autowired
+    private KepServerDataPlugin kepServerDataCollecter;
+
+    @RequestMapping(
+            value = "/sync/{thingCode}/{metricCode}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> syncRead(@PathVariable String thingCode, @PathVariable String metricCode) {
+        DataModel dm = kepServerDataCollecter.syncRead(thingCode, metricCode);
+
+        return new ResponseEntity<>(
+                ServerResponse.buildOkJson(dm.getValue())
+                , HttpStatus.OK);
+    }
+
+}
