1cbe74f8893ba7188ce9b916d8564453e515a837
jeickey <zhaojianjun@zhiguaniot.com>
2017-11-15 20:50:33 +0800

    support adhoc data load.

--- src/main/java/com/zgiot/dataengine/controller/DataReadController.java
+++ src/main/java/com/zgiot/dataengine/controller/DataReadController.java
@@ -1,7 +1,9 @@
 package com.zgiot.dataengine.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.zgiot.common.pojo.DataModel;
 import com.zgiot.common.restcontroller.ServerResponse;
+import com.zgiot.dataengine.common.queue.QueueManager;
 import com.zgiot.dataengine.dataplugin.kepserver.KepServerDataPlugin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Queue;
+
 @RestController
 @RequestMapping(value = "/data")
 public class DataReadController {
@@ -19,6 +23,13 @@ public class DataReadController {
     @Autowired
     private KepServerDataPlugin kepServerDataCollecter;
 
+    /**
+     *
+     * @param thingCode
+     * @param metricCode
+     * @return
+     * @deprecated replace to: #adhocLoadData
+     */
     @RequestMapping(
             value = "/sync/{thingCode}/{metricCode}",
             method = RequestMethod.GET,
@@ -28,6 +39,26 @@ public class DataReadController {
 
         return new ResponseEntity<>(
                 ServerResponse.buildOkJson(dm.getValue())
+                , HttpStatus.OK);
+    }
+
+    /**
+     * On demand sync load data from device and also notify upforwarders.
+     * @param thingCode
+     * @param metricCode
+     * @return
+     */
+    @RequestMapping(
+            value = "/adhocLoad/{thingCode}/{metricCode}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> adhocLoadData(@PathVariable String thingCode, @PathVariable String metricCode) {
+        DataModel dm = kepServerDataCollecter.syncRead(thingCode, metricCode);
+        Queue q = QueueManager.getQueueCollected();
+        q.add(dm);
+
+        return new ResponseEntity<>(
+                ServerResponse.buildOkJson(JSON.toJSONString(dm))
                 , HttpStatus.OK);
     }
 
