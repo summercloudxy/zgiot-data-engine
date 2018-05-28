9b648f679ff4ed0c45c8ef47136a426090355bf3
jeickey <zhaojianjun@zhiguaniot.com>
2018-05-28 19:55:44 +0800

    Fix config files for v2.1

---
+++ src/main/java/com/zgiot/dataengine/controller/MySystemInfoController.java
@@ -0,0 +1,9 @@
+package com.zgiot.dataengine.controller;
+
+import com.zgiot.common.restcontroller.SystemInfoController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MySystemInfoController extends SystemInfoController {
+}
+
